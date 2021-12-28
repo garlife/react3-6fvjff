350c6d297717a8ccb0bb232361a88340da731d0d
garlife <garlife@gmail.com>
2021-12-28 19:47:30 +0300

    tick func

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+function tick() {
+  const time = new Date().toLocaleTimeString();
+  console.log(time);
+  const element = (
+    <>
+      <h3>Date</h3>
+      <p>{time}</p>
+    </>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+setInterval(() => {
+  tick();
+}, 10000);
